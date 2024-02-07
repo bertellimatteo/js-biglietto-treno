99f63832d1b92c8b2f975a8f8dad1c8413c4adfd
bertellimatteo <lavoro.bertellimatteo@gmail.com>
2024-02-07 21:38:26 +0100

    add the cost for the base prize

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,6 +1,8 @@
 function calcoloPrezzo() {
 
   const km = parseFloat(document.getElementById('km').value);
-  const eta = parseInt(document.getElementById('age').value);
+  const age = parseInt(document.getElementById('age').value);
+
+  const basePrice = km * 0.21;
 
 }
